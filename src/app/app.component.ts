628823a51f1f4c9aa340c82ccaef2ab19a289680
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-09-05 20:26:09 -0500

    Sección 47: Componentes en Angular -> 170. Crear Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
+/* 
+app.component.ts, es un componente, se crea por defecto en el proyecto
+
+Los componentes estan formados por:
+Una serie de imports, que son modulos u objetos, que debemos importar para poder utilizarlos 
+  * Con import, importamos el modulo 'Component'
+  * Con from, indicamos el paquete que esta guardado en el package.json, en el caso de 'Component' esta guardado en el core 
+*/
 import { Component } from '@angular/core';
 
+/* 
+@Component, es un decorador, en el decorador se configura el component
+  * Con la propiedad selector, le puedo indicar con que etiqueta o directiva de la pagina se cargara este component
+  * Con la propiedad templateUrl, podremos ver la vista del componente
+  * Con la propiedad styleUrls, podremos agregar estilos CSS, por si queremos darle estilos al componente en especifico
+
+export en la clase, sirve para poder utilizar esta clase en otros archivos 
+Dentro de la clase definimos propiedades o funcionalidades que tendra la clase, en este caso tenemos una propiedad publica llamada 'title'
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aprendiendo-angular';
+  public title = 'Master de JavaScript y Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
+/*
+Importamos los dos modulos o los dos objetos, que se encargan de hacer esto, que son el BrowserModule y el NgModule 
+*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/*
+Importamos los componentes
+*/
 import { AppComponent } from './app.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
 
+/* 
+NgModule, es el decorador, que nos permite configurar los modulos
+  * Indicamos declarations, nos sirve para cargar nuestros componentes, nuestros pipes y nuestras directivas
+  * Indicamos imports, nos sirve para cargar modulos que tenga el propio framework de Angular, modulos que hayamos instalados nosotros externos, o modulos que hayamos creado nosotros
+  * Indicamos providers, nos sirve para cargar servicios
+  * Indicamos bootstrap, que es el modulo principal o el componente principal que se va a cargar
+*/
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideojuegoComponent
   ],
   imports: [
     BrowserModule
