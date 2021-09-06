cb668f9e8d1042b67e3b8b5e4597c26a4458bc08
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-09-05 21:42:50 -0500

    Sección 47: Componentes en Angular -> 171. Plantillas y propiedades

--- src/app/videojuego/videojuego.component.ts
+++ src/app/videojuego/videojuego.component.ts
@@ -2,20 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
    selector: 'videojuego',
-   template: `
-      <h2>Componente de Videojuegos</h2>
-      <ul>
-         <li>GTA</li>
-         <li>Prince of Persia</li>
-         <li>Tekken</li>
-         <li>Mario</li>
-      </ul>`
+   templateUrl: './videojuego.component.html'
 })
 export class VideojuegoComponent {
    
-   public title = '';
+   /*
+   Propiedades:
+      * Podemos definir dentro de la clase una serie de propiedades publicas, y podemos definir que tipo de dato es
+      * Podemos pasar el valor que yo guarde en estas propiedades a mi vista
+   */
+   public titulo: string;
+   public listado: string;
 
+   /*
+   Constructor:
+      * Lo mas normal, es asignarle un valor a las propiedades dentro del constructor, porque para eso esta hecho
+      * Podemos usar la propiedad titulo en la vista, se puede interpolar con las dobles llaves {{}}  
+   Interpolar: 
+      * Con las dobles llaves se imprime el valor que tenga la propiedad titulo del componente, eso se llama interpolacion, binding x interpolacion, se muestra el valor de una propiedad en la vista
+   */
    constructor(){
+      this.titulo = "Componente de Videojuegos";
+      this.listado = "Listado de los juegos mas populares";
+
       console.log("Se ha cargado el componente: videojuego.component.ts")
    }
 
