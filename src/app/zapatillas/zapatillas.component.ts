86b062df6fd685f331a2de1126d7ced1b27e221d
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-10-18 23:55:06 -0500

    Sección 52: Directiva ngClass de Angular -> 185. ngClass en atributos

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -18,8 +18,8 @@ export class ZapatillasComponent implements OnInit {
 
       //Le damos valores al array                  
       this.zapatillas = [ //Zapatillas es igual a un array
-         new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80,  true), //Aqui creamos el objeto, utilizando mi molde, utilizando mi clase
-         new Zapatilla('Nike Runner MD' , 'Nike'   , 'Negras', 60,  true),
+         new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80 , true), //Aqui creamos el objeto, utilizando mi molde, utilizando mi clase
+         new Zapatilla('Nike Runner MD' , 'Nike'   , 'Negras', 60 , true),
          new Zapatilla('Adidas Yezzy'   , 'Adidas' , 'Gris'  , 180, false),
          new Zapatilla('Nike Airmax'    , 'Nike'   , 'Rojas' , 190, true),
       ];
